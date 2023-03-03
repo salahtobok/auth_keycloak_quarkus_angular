7fc5d15fcfe2c567a76b06918453f4dd4b623330
salahtobok <salahtobok1@gmail.com>
2023-03-03 23:06:06 +0100

    Push update N°02 .

--- admin-system/src/main/webapp/admin/src/app/app.component.ts
+++ admin-system/src/main/webapp/admin/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Keycloak from "keycloak-js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'admin';
+
+
+  keycloak = new Keycloak({
+    clientId: "backend-service", url: "http://localhost:8543/",
+    "realm": "quarkus",
+});
+
+  ngOnInit(): void {
+    console.log("============================")
+    this.keycloak.init({
+      onLoad: 'login-required',
+      silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+    })
+  }
 }
