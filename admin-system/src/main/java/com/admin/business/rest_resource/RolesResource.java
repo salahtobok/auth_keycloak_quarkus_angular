3cdbb2495cdf07ccdca1e5d954eabeeb33139d5a
salahtobok <salahtobok1@gmail.com>
2023-03-04 20:19:30 +0100

    Push update N°06.

--- admin-system/src/main/java/com/admin/business/rest_resource/RolesResource.java
+++ admin-system/src/main/java/com/admin/business/rest_resource/RolesResource.java
@@ -22,19 +22,9 @@ public class RolesResource {
     Keycloak keycloak;
     Keycloak keycloak1;
 
-    Keycloak adminCli ;
-
     @PostConstruct
     public void initKeycloak() {
-         adminCli = KeycloakBuilder.builder()
-                 .serverUrl("http://localhost:8543")
-                 .realm("quarkus")
-                 .clientId("realm-management")
-                 .clientSecret("mo0yMEGOSjvg2hlYvMvUYokeXgh1Cbip")
-                 .username("salahtobok")
-                 .password("root")
-                 .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
-                .build();
+
 /*        keycloak1 = KeycloakBuilder.builder()
                 .serverUrl("http://localhost:8543")
                 .realm("quarkus")
@@ -50,28 +40,21 @@ public class RolesResource {
                 .serverUrl("http://localhost:8543")
                 .realm("quarkus")
                 .clientId("backend-service")
-                .clientSecret("76e5X5DjYZWUX0a4TsP7njBYVVfrlT6p")
+//                .clientSecret("76e5X5DjYZWUX0a4TsP7njBYVVfrlT6p")
                 .username("salahtobok")
                 .password("root")
-                .grantType(CLIENT_CREDENTIALS)
+//                .grantType(CLIENT_CREDENTIALS)
                 .build();
-//        quarkus.keycloak.admin-client=true
-//        quarkus.keycloak.admin-client.server-url=http://localhost:8081
-//        quarkus.keycloak.admin-client.realm=quarkus
-//        quarkus.keycloak.admin-client.client-id=quarkus-client
-//        quarkus.keycloak.admin-client.client-secret=secret
-//        quarkus.keycloak.admin-client.username= # remove default username
-//        quarkus.keycloak.admin-client.password= # remove default password
-//        quarkus.keycloak.admin-client.grant-type=CLIENT_CREDENTIALS
-        /*keycloak1 = KeycloakBuilder.builder()
-                .serverUrl("http://localhost:8543")
-                .realm("master")
-                .clientId("admin-cli")
+
+
+/*                worked when client is public
+        .serverUrl("http://localhost:8543")
+                .realm("quarkus")
+                .clientId("backend-service")
                 .username("salahtobok")
                 .password("root")
-                .grantType(PASSWORD)
                 .build();*/
-    }
+        }
 
 
     @GET
