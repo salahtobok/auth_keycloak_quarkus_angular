35f97745b0dda71bbdd35c4596ec3d87dbdb3286
salahtobok <salahtobok1@gmail.com>
2023-03-08 15:07:22 +0100

    Push update N°22

--- admin-system/src/main/java/com/admin/business/rest_resource/RoomResource.java
+++ admin-system/src/main/java/com/admin/business/rest_resource/RoomResource.java
@@ -14,14 +14,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/api/rooms")
 @Authenticated
 public class RoomResource {
 
-    @Inject
-    @RestClient
-    RoomServiceRest roomService;
+//    @Inject
+//    @RestClient
+//    RoomServiceRest roomService;
 
     @Inject
     Keycloak keycloak;
@@ -31,8 +32,12 @@ public class RoomResource {
     @RolesAllowed("FETCH_ROOMS")
     public List<Room> getAllRooms(){
 //        keycloak.tokenManager().getAccessToken()
+
         List<Room> rooms = new ArrayList<>();
-        rooms = roomService.getAllRooms();
+        for(int i = 1;i < 20; i++){
+            rooms.add(new Room(UUID.randomUUID().toString(),UUID.randomUUID().toString(),UUID.randomUUID().toString()));
+        }
+//        rooms = roomService.getAllRooms();
         return rooms;
     }
 }
